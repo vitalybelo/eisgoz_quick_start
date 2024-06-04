88dcf18217987918c5e0c18e36f33c2334c0ec7d
vitalybelo <vitaly.belo@gmail.com>
2024-06-04 14:05:10 +0300

    try it on ...

--- src/main/java/ru/lanit/minobr/service/quick_start/QuickStartApplication.java
+++ src/main/java/ru/lanit/minobr/service/quick_start/QuickStartApplication.java
@@ -3,8 +3,6 @@ package ru.lanit.minobr.service.quick_start;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class QuickStartApplication {
